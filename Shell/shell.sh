da980804cec14c95b2786d6247f70354f0e019fa
MAOXU LIU <maoxummbb@gmail.com>
2017-11-29 15:46:42 -0500

    added question The world of numbers

--- Shell/shell.sh
+++ Shell/shell.sh
@@ -6,3 +6,15 @@ for var in {1..50..1}
 do
 	echo $var
 done
+
+#----The World of Numbers----
+read var1
+read var2
+let sum=$var1+$var2
+let diff=$var1-$var2
+let prod=$var1*$var2
+let quot=$var1/$var2
+echo $sum
+echo $diff
+echo $prod
+echo $quot
