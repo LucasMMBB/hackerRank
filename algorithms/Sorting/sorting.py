aad2d5f36a1a145b3f97faece74671dcc620c54b
LucasMMBB <maoxummbb@gmail.com>
2018-01-07 11:42:11 -0500

    add sorting port

---
+++ algorithms/Sorting/sorting.py
@@ -0,0 +1,20 @@
+# question path
+# Algorithms/Sorting/Big Sorting
+def bigSorting(arr):
+    # Complete this function
+    for i in range(len(arr)):
+        arr[i] = int(arr[i])
+    arr.sort()
+
+    return num
+
+def bigSorting(arr):
+	tmp = map(int, arr)
+	tmp.sort()
+	return tmp
+
+def bigSorting(arr):
+	tmp = [int(num) for num in arr]
+	tmp.sort()
+	return tmp
+	
